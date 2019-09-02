1c670afdaa61e5b36299beb9ba747b8d1582f20d
peter chang <peter.chang.04@gmail.com>
2019-09-02 16:26:09 -0700

    made starCanvas a bit more natural looking, with randomized layers instead of 3 fixed layers of paralax

--- src/game/canvas/starCanvas.js
+++ src/game/canvas/starCanvas.js
@@ -13,13 +13,38 @@ const temp = {
 let canvas = null;
 let context = null;
 let stars = [];
-let stars2 = [];
-let stars3 = [];
 
-function populateStars(count) {
+function prepopulateStars() {
   temp.stars = [];
-  for (temp.i = 0; temp.i < count; temp.i++) {
-    temp.stars.push({ x: maths.random(0, 99), y: maths.random(0, 99), w: (maths.random(0, 99) / 100 * $g.viewport.pixelsPerMeter) });
+  // handful of closer looking foreground specks
+  for (temp.i = 0; temp.i < 7; temp.i++) {
+    temp.stars.push({
+      x: maths.random(0, 99),
+      y: maths.random(0, 99),
+      w: (maths.random(0, 99) / 100 * $g.viewport.pixelsPerMeter),
+      c: `rgba(255,255,255,${maths.random(60, 80) / 100})`,
+      scale: maths.random(300,450) / 100, // 3-4.5 in scale
+    });
+  }
+  // mediums distance stars
+  for (temp.i = 0; temp.i < 20; temp.i++) {
+    temp.stars.push({
+      x: maths.random(0, 99),
+      y: maths.random(0, 99),
+      w: (maths.random(0, 99) / 100 * $g.viewport.pixelsPerMeter),
+      c: `rgba(255,255,255,${maths.random(70, 90) / 100})`,
+      scale: maths.random(30,70) / 100, // .3-.7 in scale
+    });
+  }
+  // long distance stars, barely moving
+  for (temp.i = 0; temp.i < 15; temp.i++) {
+    temp.stars.push({
+      x: maths.random(0, 99),
+      y: maths.random(0, 99),
+      w: (maths.random(0, 99) / 100 * $g.viewport.pixelsPerMeter),
+      c: `rgba(255,255,255,${maths.random(40, 90) / 100})`,
+      scale: maths.random(0,20) / 100, // 0.0-0.2 in scale
+    });
   }
   return temp.stars;
 }
@@ -34,9 +59,7 @@ function init() { perf.start('starCanvas.init');
   canvas.width = $g.viewport.pixelWidth;
   canvas.height = $g.viewport.pixelHeight;
 
-  stars = populateStars(20);
-  stars2 = populateStars(15);
-  stars3 = populateStars(7);
+  stars = prepopulateStars();
   perf.stop('starCanvas.init');
 }
 
@@ -58,48 +81,46 @@ function getSectors(sectorLength) {
 
 function draw() { perf.start('starCanvas.draw');
   context.clearRect(0, 0, canvas.width, canvas.height);
-  drawStarsProjectedBySector(stars, 0.7, '#ffffffff');
-  drawStarsProjectedBySector(stars2, 0.2, '#ffffffCC');
-  drawStarsProjectedBySector(stars3, 4, '#ffffff99');
+  drawStarsProjectedBySector(stars);
   perf.stop('starCanvas.draw');
 }
 
-function drawStarsProjectedBySector(stars, scale = 1, color = '#ffffffff') { perf.start('starCanvas.drawStarsProjectedBySector');
-  const sectorLength = $g.viewport.pixelWidth / 2 / scale;
-  temp.sectors = getSectors(sectorLength);
+function drawStarsProjectedBySector(stars) { perf.start('starCanvas.drawStarsProjectedBySector');
   // sector upper right
-  temp.sectors.map((sector) => {
-    stars.map((star) => {
+  stars.map((star) => {
+    temp.sectorLength = $g.viewport.pixelWidth / 2 / star.scale;
+    temp.sectors = getSectors(temp.sectorLength);
+    temp.sectors.map((sector) => {
       // scale the star (defined as 0-99) to sectorLength
-      star.scaleX = star.x * (sectorLength / 100);
-      star.scaleY = star.y * (sectorLength / 100);
+      star.scaleX = star.x * (temp.sectorLength / 100);
+      star.scaleY = star.y * (temp.sectorLength / 100);
       // rotate depending on which sector
       if (sector[0] % 2 === 0) {
         if (sector[1] % 2 === 0) {
-          temp.pivotBySectorX = (sectorLength - star.scaleX) + (sectorLength * sector[0]);
-          temp.pivotBySectorY = star.scaleY + (sectorLength * sector[1]);
+          temp.pivotBySectorX = (temp.sectorLength - star.scaleX) + (temp.sectorLength * sector[0]);
+          temp.pivotBySectorY = star.scaleY + (temp.sectorLength * sector[1]);
         } else {
-          temp.pivotBySectorX = star.scaleY + (sectorLength * sector[0]);
-          temp.pivotBySectorY = star.scaleX + (sectorLength * sector[1]);
+          temp.pivotBySectorX = star.scaleY + (temp.sectorLength * sector[0]);
+          temp.pivotBySectorY = star.scaleX + (temp.sectorLength * sector[1]);
         }
       } else {
         if (sector[1] % 2 === 0) {
-          temp.pivotBySectorX = (sectorLength - star.scaleY) + (sectorLength * sector[0]);
-          temp.pivotBySectorY = star.scaleX + (sectorLength * sector[1]);
+          temp.pivotBySectorX = (temp.sectorLength - star.scaleY) + (temp.sectorLength * sector[0]);
+          temp.pivotBySectorY = star.scaleX + (temp.sectorLength * sector[1]);
         } else {
-          temp.pivotBySectorX = star.scaleX + (sectorLength * sector[0]);
-          temp.pivotBySectorY = (sectorLength - star.scaleY) + (sectorLength * sector[1]);
+          temp.pivotBySectorX = star.scaleX + (temp.sectorLength * sector[0]);
+          temp.pivotBySectorY = (temp.sectorLength - star.scaleY) + (temp.sectorLength * sector[1]);
         }
       }
       // get pixel to draw
       temp.viewportPixel = $g.game.myShip.getViewportPixel(temp.pivotBySectorX, temp.pivotBySectorY);
-      temp.viewportPixel.scaleX = (temp.viewportPixel.x * scale) + $g.viewport.shipPixelX - (scale * $g.viewport.shipPixelX);
-      temp.viewportPixel.scaleY = (temp.viewportPixel.y * scale) + $g.viewport.shipPixelY - (scale * $g.viewport.shipPixelY);
+      temp.viewportPixel.scaleX = (temp.viewportPixel.x * star.scale) + $g.viewport.shipPixelX - (star.scale * $g.viewport.shipPixelX);
+      temp.viewportPixel.scaleY = (temp.viewportPixel.y * star.scale) + $g.viewport.shipPixelY - (star.scale * $g.viewport.shipPixelY);
 
       if (temp.viewportPixel.scaleX > $g.viewport.pixelWidth || temp.viewportPixel.scaleX < 0) return;
       if (temp.viewportPixel.scaleY > $g.viewport.pixelHeight || temp.viewportPixel.scaleY < 0) return;
 
-      context.fillStyle = color;
+      context.fillStyle = star.c;
       context.fillRect(
         temp.viewportPixel.scaleX,
         temp.viewportPixel.scaleY,
